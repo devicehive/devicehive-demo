20bd376fb3643de321e46958d414043de4a88b05
Artem Sorokin <sorokinjef@gmail.com>
2014-12-04 19:57:27 +0200

    Fix attempt 2

--- device/app.js
+++ device/app.js
@@ -2,63 +2,65 @@
 var DevicehiveConnector = require('./devicehive-connector.js');
 
 var app = ({
-    
+
     serviceUUIDs: [
         // '70f31319a57e4eaa97bb6dcb89ccb2c5'
     ], // default: [] => all
-    
+
     start: function () {
-        var self = this;
-        DevicehiveConnector.init(function () {
-            self.subscribeNoble();
-        });
-    },
-    
-    subscribeNoble: function () {
         var self = this;
         noble.on('stateChange', function (state) {
             self.onStateChange.apply(self, [state]);
         });
-        noble.on('scanStart', this.onScanStart);
-        noble.on('scanStop', this.onScanStop);
-        noble.on('discover', function (peripheral) {
-            console.log("discover()");            
-            self.onDiscover.apply(self, [peripheral]);
-        });
     },
-    
+
     onStateChange: function (state) {
+        var self = this;
+
         console.log("stateChange(): " + state);
-        if (state === "poweredOn") {
-            noble.startScanning(this.serviceUUIDs, true);
+
+        if (state === "poweredOff") {
+
+            DevicehiveConnector.init(function () {
+                console.log("DeviceHive channel opened");
+
+                noble.on('scanStart', self.onScanStart);
+                noble.on('scanStop', self.onScanStop);
+                noble.on('discover', function (peripheral) {
+                    console.log("discover()");
+                    self.onDiscover.apply(self, [peripheral]);
+                });
+
+                noble.startScanning(self.serviceUUIDs, true);
+            });
         }
     },
-    
+
     onScanStart: function () {
         console.log('Starting scanning for peripheral');
     },
-    
+
     onScanStop: function () {
         console.log('Stopping peripheral scan');
     },
-    
+
     onDiscover: function (peripheral) {
         console.log("onDiscover()");
-        
+
         if (peripheral.advertisement.localName !== 'SensorTag') {
             return;
         }
-        
+
         if (peripheral.state !== 'disconnected') {
             return;
         }
 
-        
+
         this.subscribePeripheral(peripheral);
         peripheral.connect();
         console.log(peripheral);
     },
-    
+
     subscribePeripheral: function (peripheral) {
         console.log("subscribePeripheral()");
         var self = this;
@@ -72,16 +74,16 @@ var app = ({
             self.onServicesDiscover.apply(self, [peripheral, services]);
         });
     },
-    
+
     onConnect: function (peripheral) {
         console.log('Connected to ' + peripheral.uuid);
         peripheral.discoverServices();
     },
-    
+
     onDisconnect: function (peripheral) {
         console.log('Disconnected from ' + peripheral.uuid);
     },
-    
+
     onServicesDiscover: function (peripheral, services) {
         var self = this;
         services.forEach(function (service) {
@@ -90,14 +92,14 @@ var app = ({
             service.discoverCharacteristics();
         });
     },
-    
+
     subscribeService: function (peripheral, service) {
         var self = this;
         service.on('characteristicsDiscover', function (characteristics) {
             self.onCharacteristicsDiscover.apply(self, [peripheral, characteristics]);
         });
     },
-    
+
     onCharacteristicsDiscover: function (peripheral, characteristics) {
         var self = this;
         characteristics.forEach(function (c) {
@@ -108,7 +110,7 @@ var app = ({
             }
         })
     },
-    
+
     characteristicHandlers: {
         aa02: function (c, id) {
             c.write(new Buffer([0x01]), false, function (error) {
@@ -144,16 +146,16 @@ var app = ({
             });
         },
     },
-    
+
     onIRTempRead: function (data, isNotification, uuid) {
         console.log('Received notification from ' + uuid + ': ' + data.toString('hex'));
         var temp = this.extractTargetTemperature(data);
         console.log('Temperature = ' + temp);
         DevicehiveConnector.send('temperature', {
-            time : new Date(),
-            tag : uuid,
-            name : 'Temperature',
-            value : temp
+            time: new Date(),
+            tag: uuid,
+            name: 'Temperature',
+            value: temp
         });
     },
 
@@ -165,29 +167,29 @@ var app = ({
         var y = data.readInt8(1) / 64.0;
         var z = -1 * data.readInt8(2) / 64.0;
 
-        var sum = x*x + y*y + z*z;
+        var sum = x * x + y * y + z * z;
 
         console.log("Accelerometer: " + sum);
 
         DevicehiveConnector.send('accelerometer', {
-            time : new Date(),
-            tag : uuid,
-            name : 'Accelerometer',
-            value : sum
+            time: new Date(),
+            tag: uuid,
+            name: 'Accelerometer',
+            value: sum
         });
     },
 
     extractAmbientTemperature: function (t) {
         return t.readUInt16LE(2) / 128.0;
     },
-    
+
     extractTargetTemperature: function (t) {
-        
+
         var Vobj2 = t.readInt16LE(0);
         Vobj2 *= 0.00000015625;
         var Tdie = this.extractAmbientTemperature(t) + 273.15;
-        
-        var S0 = 5.593E-14;	// Calibration factor
+
+        var S0 = 5.593E-14; // Calibration factor
         var a1 = 1.75E-3;
         var a2 = -1.678E-5;
         var b0 = -2.94E-5;
@@ -199,7 +201,7 @@ var app = ({
         var Vos = b0 + b1 * (Tdie - Tref) + b2 * Math.pow((Tdie - Tref), 2);
         var fObj = (Vobj2 - Vos) + c2 * Math.pow((Vobj2 - Vos), 2);
         var tObj = Math.pow(Math.pow(Tdie, 4) + (fObj / S), 0.25);
-        
+
         return tObj - 273.15;
     },
 });
